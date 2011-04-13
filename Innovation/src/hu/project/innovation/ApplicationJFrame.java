2ee8937958307e837292386e421af2ab569d2aaa
stefankemp1 <stefankemp1@ca03d73f-22de-c618-d92a-c64c175a0739>
2011-04-13 11:59:38 +0000

    JFrame vanuit Gigloo gegenereert. Main methode aan Main.java toegevoegd.

--- Innovation/src/hu/project/innovation/ApplicationJFrame.java
+++ Innovation/src/hu/project/innovation/ApplicationJFrame.java
@@ -1,5 +1,29 @@
 package hu.project.innovation;
 
-public class ApplicationJFrame {
+import javax.swing.WindowConstants;
+
+public class ApplicationJFrame extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = 6858870868564931134L;
+
+	/**
+	 * Auto-generated main method to display this JFrame
+	 */
+
+	public ApplicationJFrame() {
+		super();
+		initGUI();
+	}
+
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+			// add your error handling code here
+			e.printStackTrace();
+		}
+	}
 
 }
--- Innovation/src/hu/project/innovation/Main.java
+++ Innovation/src/hu/project/innovation/Main.java
@@ -2,4 +2,8 @@ package hu.project.innovation;
 
 public class Main {
 
+	public static void main(String[] args) {
+
+	}
+
 }
