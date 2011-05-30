b65724877e8ac8eeafded508b065aaa16f7fba2a
j3ll3nl@gmail.com <j3ll3nl@gmail.com@ca03d73f-22de-c618-d92a-c64c175a0739>
2011-05-30 13:01:38 +0000

    Het eventueel toevoegen van een Jar-file aan het project

--- Innovation/src/hu/project/innovation/analyse/model/AnalyseService.java
+++ Innovation/src/hu/project/innovation/analyse/model/AnalyseService.java
@@ -4,6 +4,10 @@ import hu.project.innovation.analyse.controller.AnalyseController;
 import hu.project.innovation.configuration.model.ConfigurationService;
 import hu.project.innovation.utils.Log;
 
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -46,4 +50,14 @@ public class AnalyseService {
 
 		new Thread(p).start();
 	}
+	
+	public static void addClasspath(String s) throws Exception {
+		  File f = new File(s);
+		  URL u = f.toURI().toURL();
+		  URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+		  Class urlClass = URLClassLoader.class;
+		  Method method = urlClass.getDeclaredMethod("addURL", new Class[]{URL.class});
+		  method.setAccessible(true);
+		  method.invoke(urlClassLoader, new Object[]{u});
+		}
 }
